0c4705454918fad98ef14216436c294a8437e9d7
Richmond522 <lvqimengqilv@gmail.com>
2024-12-05 17:45:38 +1300

    feat: Implement global exception handling with logging
    - Added global exception handling to ensure consistent error responses across the application.
    - Integrated logging to capture detailed error information for debugging and monitoring.

---
+++ CreditCard.API/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+
+namespace CreditCard.API.ErrorHandling
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private static readonly NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public GlobalExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "An unhandled exception occurred.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            return context.Response.WriteAsync(new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error. Please try again later." + exception
+            }.ToString()); ;
+        }
+    }
+}

1426741ab252202b96613ae8905272998765f57c
Richmond522 <lvqimengqilv@gmail.com>
2024-12-05 17:47:43 +1300

    feat: Add exception, logging, and parameter validation filters
    - Created `ExceptionFilter` to centralize exception handling.
    - Implemented `LoggingFilter` for consistent log recording across application processes.
    - Added `ParameterValidationFilter` to validate request parameters.
    - Abstracted these functionalities into filters to maintain clean and modular code structure.

---
+++ CreditCard.API/Filters/ModelValidationFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLog;
+
+namespace CreditCard.API.Filters
+{
+    public class ModelValidationFilter : IActionFilter
+    {
+        private static readonly NLog.ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Title = "Bad Request",
+                    Status = (int)StatusCodes.Status400BadRequest,
+                    Detail = "Invalid request parameters.",
+                    Instance = context.HttpContext.Request.Path
+                };
+                
+                foreach (var key in context.ModelState.Keys)
+                {
+                    var errors = context.ModelState[key].Errors.Select(e => e.ErrorMessage).ToArray();
+                    if (errors.Length > 0)
+                    {
+                        problemDetails.Extensions["invalidParams:" + key] = errors;
+                    }
+                }
+                context.Result = new BadRequestObjectResult(problemDetails);
+                return;
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        { }
+    }
+}

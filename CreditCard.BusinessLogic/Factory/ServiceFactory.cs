b3644c1c3a705866252e3daed300105f0daafc2d
Richmond522 <lvqimengqilv@gmail.com>
2024-12-05 17:32:17 +1300

    feat: Add ServiceFactory class with string injection and extensibility support
    - Implemented `ServiceFactory` class to dynamically provide service instances using string-based injection.
    - Designed to support future extension for additional services.
    - Enhances scalability and maintains loose coupling in service management.

---
+++ CreditCard.BusinessLogic/Factory/ServiceFactory.cs
@@ -0,0 +1,16 @@
+﻿using CreditCard.BusinessLogic.Services;
+
+namespace CreditCard.BusinessLogic.Factories
+{
+    public static class ServiceFactory
+    {
+        public static ICreditCardService? Create(string serviceType)
+        {
+            switch(serviceType)
+            {
+                case "CreditCardService": return new CreditCardService();
+                default: return null;
+            }
+        }
+    }
+}

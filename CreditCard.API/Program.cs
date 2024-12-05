3554afa1c0d51c8cbd110fa1688a7e626a84fcbe
Richmond522 <lvqimengqilv@gmail.com>
2024-12-05 17:49:24 +1300

    feat: Create application entry point with configuration loading and filter/service registration
    - Implemented the main program startup entry point.
    - Added functionality to read configuration files during initialization.
    - Registered global filters, including exception handling, logging, and parameter validation.
    - Registered necessary services to support application functionality.

---
+++ CreditCard.API/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.OpenApi.Models;
+using CreditCard.API.ErrorHandling;
+using CreditCard.API.Filters;
+using System.Reflection;
+using NLog;
+using NLog.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+            // Register the Filter globally
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<LogFilter>();
+                options.Filters.Add<ExceptionFilter>();
+                options.Filters.Add<ModelValidationFilter>();
+            });
+
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Credit Card API", Version = "v1" });
+                // Add XML comments if available
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            builder.Services.AddHealthChecks();
+
+            var app = builder.Build();
+
+            app.UseMiddleware<GlobalExceptionHandler>();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapHealthChecks("/health");
+
+            app.Run();
+        }
+        catch (Exception exception)
+        {
+            logger.Error(exception, "Stopped program because of exception");
+            throw;
+        }
+        finally
+        {
+            LogManager.Shutdown();
+        }
+    }
+}

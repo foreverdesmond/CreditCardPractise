c53caca10d0ff3ca69731f5e3e07e4b9ee7234f4
Richmond522 <lvqimengqilv@gmail.com>
2024-12-05 17:28:27 +1300

    feat: Create IsValidLuhn class with core method for Luhn validation
    - Implemented the core algorithm for Luhn validation within the `IsValidLuhn` class.
    - Designed to validate credit card numbers effectively.

---
+++ CreditCard.BusinessLogic/Utilities/IsValidLuhn.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace CreditCard.BusinessLogic.Utilities
+{
+    public static class IsValidLuhn
+    {
+        public static bool Validate(string cardNumber)
+        {
+            if (string.IsNullOrWhiteSpace(cardNumber) || !cardNumber.All(char.IsDigit))
+                return false;
+
+            int sum = 0;
+            bool doubleDigit = false;
+
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = cardNumber[i] - '0';
+
+                if (doubleDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                        digit -= 9;
+                }
+
+                sum += digit;
+                doubleDigit = !doubleDigit;
+            }
+
+            return sum % 10 == 0;
+        }
+    }
+}
